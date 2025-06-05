5c923f1e3fd481936c9b48e3b1ae5bef9699c614
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-06-05 19:42:45 +0300

    Adding scroll overflow

--- taskmanager_frontend/src/page/Main.jsx
+++ taskmanager_frontend/src/page/Main.jsx
@@ -2,7 +2,7 @@ import Card from "../components/Card";
 
 export default function Main({ tasks, onDelete, onToggleDone, onEdit }) {
   return (
-    <section className="min-h-[70vh] pt-10 flex flex-col items-center bg-[#393652]">
+    <section className="max-h-[70vh] overflow-y-scroll pt-10 flex flex-col items-center bg-[#393652]">
       {tasks.map((task) => (
         <Card
           key={task.id}
